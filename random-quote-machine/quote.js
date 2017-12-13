c01e3c55da5dd199ed5ebbcf974ad0ed2a612d3a
Kimberlee <krd020@mail.usask.ca>
2017-12-12 18:33:30 -0600

    Code for random quote page

---
+++ random-quote-machine/quote.js
@@ -0,0 +1,32 @@
+var quoteContainer = document.getElementById("quote-info")
+var btn = document.getElementById("btn");
+var htmlstr = "";
+
+function getQuote() {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://kimdube.github.io/web-development/quotes.json');
+    ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
+  };
+ourRequest.send();
+};
+
+function renderHTML(data) {
+  var randomNum = Math.floor(Math.random() * 83);
+  var htmlString = data[randomNum].quote + "  -" +data[randomNum].author; 
+  quoteContainer.innerHTML = htmlString;
+  htmlstr = htmlString;
+};
+
+function sendtweet(){
+  var link =  "http://twitter.com/share?text="
+  link += htmlstr;
+  return link;
+};
+
+quotebtn.addEventListener("click", getQuote);
+tweeterbtn.addEventListener('click', function(){
+  	window.open(sendtweet());
+  	});
+document.addEventListener("DOMContentLoaded", getQuote);
